62b0dc9e018b13fa1a79c76bdc4814d6cb522703
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2023-09-01 10:04:57 +0300

    Update preplexity.py
    not tested yet

--- eval/preplexity.py
+++ eval/preplexity.py
@@ -1,18 +1,28 @@
 import transformers, torch, datasets, numpy
+import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 from typing import List 
+import argparse, os
 
 
 
-def compute_prelpexity(model_id: str, dataset: List) -> tuple:
+def compute_prelpexity(model_id: str, testset: str) -> tuple:
+
+  """
+    Input:
+        model_id: HF model id
+        testset: a path to testset in CSV format
+    Return:
+        a tuple with all preplexity socres in a list and their averge 
+    """
 
   model = AutoModelForCausalLM.from_pretrained(model_id)
   tokenizer = AutoTokenizer.from_pretrained(model_id)
 
   ppls = []
   
-  for text in dataset: 
+  for text in testset: 
     input = tokenizer(text, return_tensors='pt')
     loss = model(input_ids = input['input_ids'], 
               labels = input['input_ids']).loss
@@ -22,3 +32,24 @@ def compute_prelpexity(model_id: str, dataset: List) -> tuple:
   avg = numpy.average(ppls)
   avg = round(avg, 2)
   return (ppls, avg)
+
+
+
+
+parser = argparse.ArgumentParser(description = "compute preplexity")
+parser.add_argument("model", type=str, help="huggingface model id")
+parser.add_argument("testset", type=str, help="path to testset in CSV file fromat with the data in the first col")
+parser.add_argument("--hf_dataset", type=str, help="hugging face dataset id")
+
+args = parser.parse_args()
+
+
+def main():
+
+    testset_path = os.path.join(args.testset)
+    df = pd.read_csv(testset_path)
+    compute_prelpexity(args.model, df[df.columns[0]])
+
+
+if __name__ == '__main__':
+    main()
